3d1abea1f172a3e410fb9152a73f637b3d5c7401
jay-menorca <zai_menorca@yahoo.com>
2013-10-22 17:40:52 -0400

    commiting exercise6 changes

---
+++ exercise6.rb
@@ -0,0 +1,16 @@
+
+class Grocery
+	@@grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+	def printList
+		@@grocery_list.each do |x|
+			puts "* #{x}"	
+		end	
+	end
+
+	myList = Grocery.new
+	#myList.printList
+
+	@@grocery_list << "rice"
+	myList.printList
+end
