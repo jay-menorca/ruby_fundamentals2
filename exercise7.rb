db9d093a48eb2ce3071ed3a7f5394c4f12e1c1b8
jay-menorca <zai_menorca@yahoo.com>
2013-10-23 06:55:20 -0400

    updated exer 7

--- exercise7.rb
+++ exercise7.rb
@@ -10,10 +10,31 @@ class HashExercise
 	def printStudents
 		puts "\nNo. of students for each cohort:"
 		@@students.each do |cohort, count|
-			puts "#{cohort} : #{count}"
+			puts "#{cohort}: #{count}"
+		end
+	end
+
+	def increaseCohort(percentVal)
+		puts "\nIncreasing all cohort count by #{percentVal}%: "
+		@@students.each do |cohort, count|
+			numToAdd = count * (percentVal.to_f/100)
+			newCount = numToAdd.to_i + count
+			@@students[cohort] = newCount
 		end
 	end
 
 	he = HashExercise.new
 	he.printStudents
+
+	puts "\n Adding Cohort 4"
+	@@students["cohort4"] = 43
+	he.printStudents
+
+	puts "\n Using keys method"
+	cohortNamesArr = @@students.keys
+	puts "#{cohortNamesArr.join(", ")}"
+
+	he.increaseCohort(5)
+	he.printStudents
+
 end
